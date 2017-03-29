00d3cefa5cbee3b283825b54ab952b3d60214fae
Schorzz <Uh2stos!>
2017-03-29 15:50:01 +0200

    Neue Klasse "Track", Playlist verwaltet nun nur

---
+++ src/player/Track.java
@@ -0,0 +1,40 @@
+package player;
+
+import java.io.File;
+import java.net.URI;
+
+import javafx.scene.media.Media;
+import javafx.util.Duration;
+
+public class Track {
+
+	private File file;
+	private String artist = "";
+	private String titel = "";
+	private Media media;
+	
+	
+	public Track(File file) {
+		if(file == null){
+			return;
+		}
+		this.file=file;
+		artist = file.getName();
+		media = new Media(file.toURI().toString());
+		
+	}
+	public String getArtist(){
+		return artist;
+	}
+	public String getTitel(){
+		return titel;
+	}
+	public Duration getDuration(){
+		return media.getDuration();
+	}
+	public String toString(){
+		return artist+" - "+titel;
+		
+	}
+	
+}
--- src/player/playlist/Playlist.java
+++ src/player/playlist/Playlist.java
@@ -1,7 +1,10 @@
 package player.playlist;
 
+import java.io.File;
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,67 +14,83 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import player.Track;
 
 public class Playlist {
 
-	private double xOffset;
-	private double yOffset;
-	private static Stage stage = new Stage();
-	
-	
-	//Die playlist verwaltet die Oberservable list, gibt den momentan markierten track, den vorgaenger sowie nen nachfolger..
+	// private double xOffset;
+	// private double yOffset;
+	// private static Stage stage = new Stage();
+
+	private ObservableList<Track> playlist = FXCollections.observableArrayList();
+
+	// Die playlist verwaltet die Oberservable list, gibt den momentan
+	// markierten track, den vorgaenger sowie nen nachfolger..
 	public Playlist() {
-		
-		//Die GUI komponenten hieraus entfernen
-		
 
-		stage.initStyle(StageStyle.UNDECORATED);
-		
-		AnchorPane root;
-		
-		try {
-			root = (AnchorPane)FXMLLoader.load(getClass().getResource("ui\\Playlist.fxml"));
-			
-			root.setOnMousePressed(new EventHandler<MouseEvent>() {
-	          
-			
+		// Die GUI komponenten hieraus entfernen
 
-				@Override
-	            public void handle(MouseEvent event) {
-	                xOffset = event.getSceneX();
-	                yOffset = event.getSceneY();
-	            }
-	        });
-			
-	        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent event) {
-	                stage.setX(event.getScreenX() - xOffset);
-	                stage.setY(event.getScreenY() - yOffset);
-	            }
-	        });
-			
-			
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("ui\\application.css").toExternalForm());
-			stage.setScene(scene);
-			stage.show();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		
+		// stage.initStyle(StageStyle.UNDECORATED);
+		//
+		// AnchorPane root;
+		//
+		// try {
+		// root =
+		// (AnchorPane)FXMLLoader.load(getClass().getResource("ui\\Playlist.fxml"));
+		//
+		// root.setOnMousePressed(new EventHandler<MouseEvent>() {
+		//
+		//
+		//
+		// @Override
+		// public void handle(MouseEvent event) {
+		// xOffset = event.getSceneX();
+		// yOffset = event.getSceneY();
+		// }
+		// });
+		//
+		// root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+		// @Override
+		// public void handle(MouseEvent event) {
+		// stage.setX(event.getScreenX() - xOffset);
+		// stage.setY(event.getScreenY() - yOffset);
+		// }
+		// });
+		//
+		//
+		// Scene scene = new Scene(root);
+		// scene.getStylesheets().add(getClass().getResource("ui\\application.css").toExternalForm());
+		// stage.setScene(scene);
+		// stage.show();
+		//
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+
+	}
+	// public static Stage getStage() {
+	// return stage;
+	// }
+
+
+	public void addTrack(File newFile) {
+		Track temp = new Track(newFile);
+		playlist.add(temp);
+	}
+
+	public void removeTrack(Track toDel) {
+
+		playlist.remove(toDel);
 		
 	}
-	public static Stage getStage() {
-		return stage;
+	public Integer getIndex(Track track){
+		return playlist.lastIndexOf(track)+1;
 	}
-	
-	
-	
+	public Track getTrackFromIndex(int index){
+		return playlist.get((index%playlist.size())-1);
+		
+	}
+
 }

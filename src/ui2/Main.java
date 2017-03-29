1c57eeb546a2470ffe18cfd042720e202effc072
Schorzz <Uh2stos!>
2017-03-29 22:57:17 +0200

    -

--- src/ui2/Main.java
+++ src/ui2/Main.java
@@ -1,10 +1,23 @@
 package ui2;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application{
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+//		primaryStage.initStyle(StageStyle.UNDECORATED);
+		
+		AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Player.fxml"));
+		Scene scene = new Scene(root);
+//		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 
 }
--- src/ui2/SampleController.java
+++ src/ui2/SampleController.java
@@ -1,5 +1,16 @@
 package ui2;
 
-public class SampleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SampleController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
